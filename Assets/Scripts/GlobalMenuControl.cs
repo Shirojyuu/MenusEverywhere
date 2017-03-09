f7caeb7b12095fd462828751fb70534895045769
Alec Day <shirojyuu@gmail.com>
2017-03-09 00:49:55 -0500

    Submenu support added!

---
+++ Assets/Scripts/GlobalMenuControl.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlobalMenuControl : MonoBehaviour {
+    [SerializeField]
+    private MenuSystem[] submenus;
+
+    [SerializeField]
+    private int menuLevel = 0;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		for(int i = 0 ; i < submenus.Length; i++)
+        {
+            if (i == menuLevel)
+                submenus[i].enableMovement = true;
+
+            else
+                submenus[i].enableMovement = false;
+        }
+	}
+
+    public void ChangeMenuLevel(int level)
+    {
+        menuLevel = level;
+    }
+}
--- Assets/Scripts/MenuSystem.cs
+++ Assets/Scripts/MenuSystem.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections;
+﻿//2017 Alec Day
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,11 +9,16 @@ public class MenuSystem : MonoBehaviour {
     private MenuItem[] items;
     [SerializeField]
     private int selectIndex = 0;
+    [SerializeField]
+    private GlobalMenuControl parentController;
+
+    private string selectedAction;
 
     public AudioSource source;
     public AudioClip cursorSound;
     public AudioClip selectSound;
 
+    public bool enableMovement = true;
 	// Use this for initialization
 	void Start () {
 		
@@ -19,16 +26,21 @@ public class MenuSystem : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update () {
-        if (Input.GetButtonDown("DPad Down") || Input.GetAxis("DU-Axis") > 0)
-        {
-            source.PlayOneShot(cursorSound);
-            selectIndex++;
-        }
 
-        if (Input.GetButtonDown("DPad Up") || Input.GetAxis("DU-Axis") < 0)
+        //Navigation
+        if (enableMovement)
         {
-            source.PlayOneShot(cursorSound);
-            selectIndex--;
+            if (Input.GetButtonDown("DPad Down") || Input.GetAxis("DU-Axis") > 0)
+            {
+                source.PlayOneShot(cursorSound);
+                selectIndex++;
+            }
+
+            if (Input.GetButtonDown("DPad Up") || Input.GetAxis("DU-Axis") < 0)
+            {
+                source.PlayOneShot(cursorSound);
+                selectIndex--;
+            }
         }
 
         //Select bounds
@@ -45,6 +57,7 @@ public class MenuSystem : MonoBehaviour {
         }
 
         items[selectIndex].highlighted = true;
+        selectedAction = items[selectIndex].GetAction();
 
         if (selectIndex == 0)
             items[selectIndex + 1].highlighted = false;
@@ -56,6 +69,26 @@ public class MenuSystem : MonoBehaviour {
         {
             items[selectIndex + 1].highlighted = false;
             items[selectIndex - 1].highlighted = false;
+        }
+
+        //Selection of a menu item
+        if(Input.GetButtonDown("Fire1"))
+        {
+            source.PlayOneShot(selectSound);
+            switch(selectedAction)
+            {
+                case "Equipment":
+                    parentController.ChangeMenuLevel(1);
+                    break;
+
+                case "Abilities":
+                    parentController.ChangeMenuLevel(2);
+                    break;
+
+                case "Settings":
+                    parentController.ChangeMenuLevel(3);
+                    break;
+            }
         }
 	}
 }
