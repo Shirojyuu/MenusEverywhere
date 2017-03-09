98599260e77f846b24bc602e01bf96fc2eaacf25
Alec Day <shirojyuu@gmail.com>
2017-03-08 19:43:02 -0500

    Initial Commit

---
+++ Assets/Scripts/MenuItem.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuItem : MonoBehaviour {
+    [SerializeField]
+    private Color selectColor;
+    [SerializeField]
+    private string actionOnSelect;
+    [SerializeField]
+    private Image itemBackground;
+    [SerializeField]
+    private Text itemText;
+    [SerializeField]
+    private string displayOption;
+    public bool highlighted;
+    
+
+	// Use this for initialization
+	void Start () {
+        itemText.text = displayOption;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(highlighted)
+        {
+            itemBackground.color = selectColor;
+        }
+
+        else
+        {
+            itemBackground.color = Color.white;
+        }
+	}
+
+    public string GetAction()
+    {
+        return actionOnSelect;
+    }
+
+    public void ChangeItemText(string newText)
+    {
+        itemText.text = newText;
+    }
+}
---
+++ Assets/Scripts/MenuSystem.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuSystem : MonoBehaviour {
+    [SerializeField]
+    private MenuItem[] items;
+    [SerializeField]
+    private int selectIndex = 0;
+
+    public AudioSource source;
+    public AudioClip cursorSound;
+    public AudioClip selectSound;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetButtonDown("DPad Down") || Input.GetAxis("DU-Axis") > 0)
+        {
+            source.PlayOneShot(cursorSound);
+            selectIndex++;
+        }
+
+        if (Input.GetButtonDown("DPad Up") || Input.GetAxis("DU-Axis") < 0)
+        {
+            source.PlayOneShot(cursorSound);
+            selectIndex--;
+        }
+
+        //Select bounds
+        if(selectIndex < 0)
+        {
+            items[selectIndex + 1].highlighted = false;
+            selectIndex = items.Length - 1;
+        }
+
+        if(selectIndex > items.Length - 1)
+        {
+            items[selectIndex - 1].highlighted = false;
+            selectIndex = 0;
+        }
+
+        items[selectIndex].highlighted = true;
+
+        if (selectIndex == 0)
+            items[selectIndex + 1].highlighted = false;
+
+        if (selectIndex == items.Length - 1)
+            items[selectIndex - 1].highlighted = false;
+
+        if (selectIndex > 0 && selectIndex < items.Length - 1)
+        {
+            items[selectIndex + 1].highlighted = false;
+            items[selectIndex - 1].highlighted = false;
+        }
+	}
+}
